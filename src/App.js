9124e793b666c4131de0c2b5a929e1b5dab7780e
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-16 15:35:18 +0600

    added bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="buttons hero ">
+        <button className="button is-primary">Primary</button>
+        <button className="button is-link">Link</button>
+        <button className="button is-info">Info</button>
+        <button className="button is-success">Success</button>
+        <button className="button is-warning">Warning</button>
+        <button className="button is-danger">Danger</button>
+        <button className="button is-black">Black</button>
+        <button className="button is-white">White</button>
+        <button className="button is-dark">Dark</button>
+        <button className="button is-light">Light</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
